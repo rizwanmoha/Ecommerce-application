267a6b402d9ed57e22541c43b8c7e9d0c12e981b
Mohd Rizwan <105350689+rizwanmoha@users.noreply.github.com>
2023-03-23 02:17:58 +0530

    Updated

--- server.js
+++ server.js
@@ -7,13 +7,18 @@ import authRoutes from './routes/authRoute.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import cors from "cors";
-import path from 'path'
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 
 dotenv.config();
 
 
 connectDB();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.use(express.json())
